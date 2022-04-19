c8b5dbd603f2b38eef7952e15c4c5f8da16ce029
Surabhi <surabhi.ajin@research.iiit.ac.in>
2022-04-19 23:08:54 +0530

    removed bug

--- src/frontend-2/argon-dashboard-react-master/src/views/examples/Maps.js
+++ src/frontend-2/argon-dashboard-react-master/src/views/examples/Maps.js
@@ -486,40 +486,32 @@ const MapWrapper = () => {
 
   const handleChange = (event) => {
     setBody(event.target.value)
-    console.log(body)
-    console.log("hi")
-    console.log(body.toUpperCase())
-
-    if (body.toUpperCase() === "LAKE") {
-      console.log("bruh")
-
-      console.log(lakeData)
-
-
-      //draw polygon 
-
-      const polygon = new google.maps.Polygon({
-        paths: lakeData,
-        strokeColor: "#FF0000",
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: "#FF0000",
-        fillOpacity: 0.35,
-      });
-
-      //form the polygon
-      polygon.setMap(google.maps.Map);
-    }
-
-    if (body.toUpperCase() === "STEPWELL") {
-
-      console.log("whyyyyyy")
-
-      //draw polygon
-
-
-    }
   };
+
+  if (body.toUpperCase() === "LAKE") {
+
+    console.log(lakeData)
+
+
+    //draw polygon 
+
+    const polygon = new google.maps.Polygon({
+      paths: lakeData,
+      strokeColor: "#FF0000",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "#FF0000",
+      fillOpacity: 0.35,
+    });
+
+    //form the polygon
+    polygon.setMap(google.maps.Map);
+  }
+
+  if (body.toUpperCase() === "STEPWELL") {
+    //draw polygon
+
+  }
 
 
 
